fd140360cb899e2b4145bb452dfab4eba257ceee
DBelwnhs <DBelwnhs@gmail.com>
2022-09-20 01:00:37 +0300

    step-20

--- lib/services/auth/cloud/firebase_cloud_storage.dart
+++ lib/services/auth/cloud/firebase_cloud_storage.dart
@@ -14,7 +14,7 @@ class FirebaseCloudStorage {
     }
   }
 
-  Future<void> updateNore({
+  Future<void> updateNote({
     required String documentId,
     required String text,
   }) async {
@@ -41,26 +41,24 @@ class FirebaseCloudStorage {
           )
           .get()
           .then(
-            (value) => value.docs.map(
-              (doc) {
-                return CloudNote(
-                  documentId: doc.id,
-                  ownerUserId: doc.data()[ownerUserIdFieldName] as String,
-                  text: doc.data()[textFileName] as String,
-                );
-              },
-            ),
+            (value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc)),
           );
     } catch (e) {
       throw CouldNotGetAllNotesException();
     }
   }
 
-  void createNewNote({required String ownerUserId}) async {
-    await notes.add({
+  Future<CloudNote> createNewNote({required String ownerUserId}) async {
+    final document = await notes.add({
       ownerUserIdFieldName: ownerUserId,
       textFileName: '',
     });
+    final fetchedNote = await document.get();
+    return CloudNote(
+      documentId: fetchedNote.id,
+      ownerUserId: ownerUserId,
+      text: 'text',
+    );
   }
 
   static final FirebaseCloudStorage _shared =
